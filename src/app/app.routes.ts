64f0047828edc492504ac4bd413bd7f0b84d9571
tusharthakur003 <tusharthakur3136@gmail.com>
2025-10-04 13:13:55 +0530

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component:AppComponent
+    }, {
+        path: "hii",
+        component:AppComponent
+    }
+];
