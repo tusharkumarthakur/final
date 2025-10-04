214874eb8b4c89d2ce234b607a95f5e4727677c1
tusharthakur003 <tusharthakur3136@gmail.com>
2025-10-04 13:30:00 +0530

    router _redirects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,6 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component:AppComponent
-    }, {
-        path: "hii",
-        component:AppComponent
-    }
+  { path: '', component: AppComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
